7049ee1ab3be4e5c580ffeaa7dd1abbbf0ae441d
Mostafa Sadek <thunder_axe_90@hotmail.com>
2015-08-19 13:15:53 -0400

    Finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -39,6 +39,12 @@ test "email addresses should be unique" do
 	@user.save
 	assert_not duplicate_user.valid?
 end
+test "email addresses should be saved as lower-case" do
+	mixed_case_email = "Foo@ExAMPle.CoM"
+	@user.email = mixed_case_email
+	@user.save
+	assert_equal mixed_case_email.downcase, @user.reload.email
+end
 test "password should be present (nonblank)" do
 	@user.password = @user.password_confirmation = " " * 6
 	assert_not @user.valid?
